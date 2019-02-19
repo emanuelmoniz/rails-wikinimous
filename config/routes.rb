951a258136976faa98ab8bfb4af4aeab96bbf52b
Emanuel Moniz <emanuelandremoniz@gmail.com>
2019-02-19 17:11:33 +0000

    half done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'articles', to: 'articles#index'
+  # get 'articles/:id/edit', to: 'articles#edit'
+  # get 'articles/new', to: 'articles#new', as: :new_article
+  # get 'articles/:id', to: 'articles#show', as: :article
+
+  # post 'restaurants', to: 'restaurants#create'
+
+  # patch 'articles/:id', to: 'articles#update'
+
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+  resources :articles
 end
